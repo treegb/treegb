7594d855f47a4ff45d498a915f9106bd98fda0cb
Debian Live user <user@localhost.localdomain>
2017-07-25 15:53:21 +0000

    <coreNote> add some navigation purpose strip buttons and functions at left side.

--- js/js_coreNote_001.js
+++ js/js_coreNote_001.js
@@ -14,6 +14,19 @@ function addLoadEvent(func) {
 }
 
 
+/* ** Get viewport dimension and always listen for any changes. */
+var viewportwidth;
+var viewportHeight;
+
+function getViewportWidthHeight() {
+    viewportWidth = Math.min(document.documentElement.clientWidth, window.innerWidth || 0);
+    viewportHeight = Math.min(document.documentElement.clientHeight, window.innerHeight || 0); 
+}
+
+addLoadEvent(getViewportWidthHeight);
+window.addEventListener("resize", getViewportWidthHeight);
+
+
 var eyeIndex = document.querySelectorAll('.eyeIndex');
 
 for (var i=0 ; i<eyeIndex.length ; i++) {
@@ -138,4 +151,185 @@ function extractIframeConent () {
 };
 
 addLoadEvent(extractIframeConent);
+
+
+
+
+/* ** Create <liScrollJumpBtn>. When clicking this narrow button on the right side of scrren, viewport will pop out "option box" and you can chose to jump to given <li> top or bottom. This give user some navigation convenience. */
+/* ** Global variable to save status. */
+
+var liScrollJumpClickLiRgist1;
+var liScrollJumpClickLiAbsPosRgist1;
+var scrollYRgist1;
+/* ** Set to null is important since it tell later function this position is not set yet. */
+var scrollYRgist2 = null;
+var scrollYRgist3 = 0;
+var scrollYRgist4 = 0;
+var scrollYRgist5 = 0;
+
+function liScrollJump() {
+    /* ** Create <liScrollJumpBtn> DOM structure. */
+    var liAll = document.querySelectorAll(".section1 li");
+    
+    function getScrollY () {
+        /* ** I get this code from here : https://stackoverflow.com/questions/19618545/body-scrolltop-vs-documentelement-scrolltop-vs-window-pagyoffset-vs-window-scrol */
+        /* <window.scrollY> should return current document position (body scroll bar position) (viewport top left to body top left distance), but I don't know why it don't work in my firefox. */
+        return window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    }
+    
+    /* ** Get current clicked li element absolute position and also current document scrollY position. */
+    function getElmAbslPos(elm) {
+        scrollYRgist1 = getScrollY();
+        /* ** <getBoundingClientRect().top> is distance that specific element relative to viewport top left position.
+           For example, if viewport is at 30% document position, and given element is at 46% document position, the <getBoundingClientRect().top> will return 16%. (actually it will return a interger, and the unit is px).
+           This code need to cauculate here because user might move (scroll) the viewport even when <jumpOptMain> is poping out (desktop). */
+        var rltPos = elm.getBoundingClientRect().top;
+        /* ** Calculate element absolute position based on <elmRltPos>. */
+        liScrollJumpClickLiAbsPosRgist1 = scrollYRgist1 + rltPos;
+    }
+    
+    var i = 0;
+
+    for (; i < liAll.length ; i++) {
+        var liScrollJumpBtn = document.createElement("div");
+        liScrollJumpBtn.classList.add("liScrollJumpBtn");
+        liAll[i].appendChild(liScrollJumpBtn);
+    }
+    
+    /* ** Create <jumpOptMain> and corresponding DOM structure. */
+    var jumpOptMain = document.createElement("div");
+    jumpOptMain.classList.add("jumpOptMain");
+    jumpOptMain.classList.add("hide");
+    jumpOptMain.innerHTML =
+        '<div class="jumpOptAbrt jumpOptBackAbrt">' +
+        '    <img class="icon glow" src="../img/170724_202104.svg">' +
+        '    <img class="icon dim" src="../img/170724_202109.svg">' +
+        '</div>' +
+        '<div class="jumpOptBack jumpOptBackAbrt">' +
+        '    <img class="icon glow" src="../img/170724_202103.svg">' +
+        '    <img class="icon dim" src="../img/170724_202108.svg">' +
+        '</div>' +
+        '<div class="jumpOptUp jumpOptUpDown">' +
+        '    <img class="icon glow" src="../img/170724_202101.svg">' +
+        '    <img class="icon dim" src="../img/170724_202106.svg">' +
+        '</div>' +
+        '<div class="jumpOptDown jumpOptUpDown">' +
+        '    <img class="icon glow" src="../img/170724_202101.svg">' +
+        '    <img class="icon dim" src="../img/170724_202106.svg">' +
+        '</div>' +
+        '<div class="jumpOptBmk jumpOptBmkJump jumpOptBmk1Jump">' +
+        '    <p class="textPcent">0<span class="textPcentPcentSign">%</span></p>' +
+        '    <p class="textDscrpt">Jump to bookmark 1</p>' +
+        '</div>' +
+        '<div class="jumpOptBmk jumpOptBmkSave jumpOptBmk1Save">' +
+        '    <div class="iconCtn">' +
+        '        <img class="icon glow" src="../img/170724_202105.svg">' +
+        '        <img class="icon dim" src="../img/170724_202110.svg">' +
+        '    </div>' +
+        '    <p class="textDscrpt">Save to bookmark 1</p>' +
+        '</div>' +
+        '<div class="jumpOptBmk jumpOptBmkJump jumpOptBmk2Jump">' +
+        '    <p class="textPcent">0<span class="textPcentPcentSign">%</span></p>' +
+        '    <p class="textDscrpt">Jump to bookmark 2</p>' +
+        '</div>' +
+        '<div class="jumpOptBmk jumpOptBmkSave jumpOptBmk2Save">' +
+        '    <div class="iconCtn">' +
+        '        <img class="icon glow" src="../img/170724_202105.svg">' +
+        '        <img class="icon dim" src="../img/170724_202110.svg">' +
+        '    </div>' +
+        '    <p class="textDscrpt">Save to bookmark 2</p>' +
+        '</div>' +
+        '<div class="jumpOptBmk jumpOptBmkJump jumpOptBmk3Jump">' +
+        '    <p class="textPcent">0<span class="textPcentPcentSign">%</span></p>' +
+        '    <p class="textDscrpt">Jump to bookmark 3</p>' +
+        '</div>' +
+        '<div class="jumpOptBmk jumpOptBmkSave jumpOptBmk3Save">' +
+        '    <div class="iconCtn">' +
+        '        <img class="icon glow" src="../img/170724_202105.svg">' +
+        '        <img class="icon dim" src="../img/170724_202110.svg">' +
+        '    </div>' +
+        '    <p class="textDscrpt">Save to bookmark 3</p>' +
+        '</div>';
+
+    document.body.appendChild(jumpOptMain);
+    
+
+    /* ** Applying <.section1> <addEventListener("click", ... )> some function, to detect current scroll position and clicked object first, for later use.  */
+    var section1All = document.querySelectorAll(".section1");
+    var i = 0;
+
+    for (; i < section1All.length ; i++) {
+        section1All[i].addEventListener("click",
+            function(a) {
+                if (a.target.classList.contains("liScrollJumpBtn")) {
+                    var btn = a.target;
+                    liScrollJumpClickLiRgist1 = btn.parentNode;
+                    getElmAbslPos(liScrollJumpClickLiRgist1);
+                    jumpOptMain.classList.remove("hide");
+                }
+            }
+        );
+    }
+    
+    
+    /* ** Applying <jumpOptMain> <addEventListener("click", ... )> and this feature function. */
+    document.querySelector(".jumpOptMain").addEventListener("click",
+        function(a) {
+            /* ** Set some "padding" so the screen will not clip off top of the page information. Unit is px.
+               Example - If set to <viewportHeight * (35/100);> = pad 35% of screen height. */
+            var padTop = viewportHeight * (50/100);
+            var textPcentString = "<span class='textPcentPcentSign'>%</span>";
+            
+            /* ** This adjusting value will make "percent value indicator" more accurate, when scroll to page bottom, percent value should be exactly "100%", not less than 100% (due to screen have some height and scrollY calculate from top left of screen). */
+            function adjustDocElmOfstHeight(scrollY) {
+                return Math.round(scrollY / (document.documentElement.offsetHeight - viewportHeight) * 100);
+            }
+
+            /* ** Function of pressing different button. */
+            if (a.target.classList.contains("jumpOptUp") || document.querySelector(".jumpOptUp").contains(a.target)) {
+                window.scrollTo(0, liScrollJumpClickLiAbsPosRgist1 - padTop);
+                scrollYRgist2 = scrollYRgist1;
+            } else if (a.target.classList.contains("jumpOptDown") || document.querySelector(".jumpOptDown").contains(a.target)) {
+                window.scrollTo(0, liScrollJumpClickLiAbsPosRgist1 + liScrollJumpClickLiRgist1.clientHeight - padTop);
+                scrollYRgist2 = scrollYRgist1;
+            } else if (a.target.classList.contains("jumpOptBack") || document.querySelector(".jumpOptBack").contains(a.target)) {
+                if (scrollYRgist2 === null) {
+                    /* ** Do nothing. */
+                } else {
+                    window.scrollTo(0, scrollYRgist2);
+                }
+            } else if (a.target.classList.contains("jumpOptAbrt") || document.querySelector(".jumpOptAbrt").contains(a.target)) {
+                /* ** Do nothing. */
+            /* ** Bmk1. */
+            } else if (a.target.classList.contains("jumpOptBmk1Jump") || document.querySelector(".jumpOptBmk1Jump").contains(a.target)) {
+                window.scrollTo(0, scrollYRgist3);
+                scrollYRgist2 = scrollYRgist1;
+            } else if (a.target.classList.contains("jumpOptBmk1Save") || document.querySelector(".jumpOptBmk1Save").contains(a.target)) {
+                scrollYRgist3 = getScrollY();
+                /* ** Custom display string. */
+                document.querySelector(".jumpOptBmk1Jump .textPcent").innerHTML = adjustDocElmOfstHeight(scrollYRgist3).toString() + textPcentString;
+            /* ** Bmk2. */
+            } else if (a.target.classList.contains("jumpOptBmk2Jump") || document.querySelector(".jumpOptBmk2Jump").contains(a.target)) {
+                window.scrollTo(0, scrollYRgist4);
+                scrollYRgist2 = scrollYRgist1;
+            } else if (a.target.classList.contains("jumpOptBmk2Save") || document.querySelector(".jumpOptBmk2Save").contains(a.target)) {
+                scrollYRgist4 = getScrollY();
+                /* ** Custom display string. */
+                document.querySelector(".jumpOptBmk2Jump .textPcent").innerHTML = adjustDocElmOfstHeight(scrollYRgist4).toString() + textPcentString;
+            /* ** Bmk3. */
+            } else if (a.target.classList.contains("jumpOptBmk3Jump") || document.querySelector(".jumpOptBmk3Jump").contains(a.target)) {
+                window.scrollTo(0, scrollYRgist5);
+                scrollYRgist2 = scrollYRgist1;
+            } else if (a.target.classList.contains("jumpOptBmk3Save") || document.querySelector(".jumpOptBmk3Save").contains(a.target)) {
+                scrollYRgist5 = getScrollY();
+                /* ** Custom display string. */
+                document.querySelector(".jumpOptBmk3Jump .textPcent").innerHTML = adjustDocElmOfstHeight(scrollYRgist5).toString() + textPcentString;
+            }
+            
+            jumpOptMain.classList.add("hide");
+        }
+    );
+}
+
+addLoadEvent(liScrollJump);
 
